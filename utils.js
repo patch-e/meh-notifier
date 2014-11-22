1fba9777987f5228672660242e5be39003d53594
patch-e <p.crager@gmail.com>
2014-11-21 21:20:36 -0500

    Shared utils with beers app

--- utils.js
+++ utils.js
@@ -1,6 +1,6 @@
 /*
 utils.js
-Module that provides utility functions to meh.js
+Module that provides utility functions to mccrager nodejs apps
 
 Copyright (c) 2014
 
@@ -15,6 +15,43 @@ module.exports = {
 		// only invoke trim() if a string was passed in
 		if (typeof s === 'string') { 
 			s = s.trim().replace('\n', '').replace('\t', '').replace(/\s+/g,' ');
+		}
+		return s;
+	},
+
+	// returns a "kicked" beer object
+	getKickedBeer: function(i) {
+		return {
+			number: i,
+			name: 'KICKED!',
+			addl: null,
+			brewery: null,
+			style: null,
+			abv: null,
+			kicked: true
+		};
+	},
+
+	// parses the "ABV" text to return a floating point value
+	parseABV: function(s) {
+		if ( (typeof s === 'string') && (s.lastIndexOf('ABV:', 0) === 0) ) { 
+			s = s.replace('ABV:', '').replace(' ', '').replace('%', '');
+		}
+		return parseFloat(s);
+	},
+
+	// parses the "last updated" text to remove the prefix label
+	parseLastUpdated: function(s) {
+		if ( (typeof s === 'string') && (s.lastIndexOf('Last Update:', 0) === 0) ) { 
+			s = s.replace('Last Update:', '').replace(' ', '');
+		}
+		return s;
+	},
+
+	// parses the "title" text to remove any beer prefixes
+	parseTitle: function(s) {
+		if ( (typeof s === 'string') && (s.lastIndexOf('Beers', 0) === 0) ) { 
+			s = s.replace('Beers', '').replace(' ', '');
 		}
 		return s;
 	}
