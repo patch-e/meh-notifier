d7b4723a298a9e4ed8b6c44ddf003366f9153cc6
patch-e <p.crager@gmail.com>
2014-11-21 21:18:25 -0500

    Rewrote most of it :)

--- meh.js
+++ meh.js
@@ -8,51 +8,90 @@ Patrick Crager
 
 */
 
-var nodemailer = require('nodemailer'),
+var http = require('http'),
+    nodemailer = require('nodemailer'),
     request = require('request'),
-		cheerio = require('cheerio'),
-		console = require('clim')(),
+    cheerio = require('cheerio'),
+    console = require('clim')('meh'),
+    later = require('later'),
     utils = require('./utils');
 
-request('http://meh.com', function (error, response, html) {
-	if (!error && response.statusCode === 200) {
-    // meh parsing
-		var $ = cheerio.load(html);
-    var buyButton = $('div#hero-buttons button.buy-button');
-    var subject = $('section.features h2').text(),
-        body  = $('div#price-check').html();
-        body += '<div><a href="' + buyButton.attr('href') + '">' + utils.trim(buyButton.text()) + '</a></div>';
-        body += $('section.features').html();
-        body += $('section.story').html();
-
-    // create e-mail message
-    var mailOptions = {
-        from: 'meh-notifier-bot <noreply@meh.com>',
-        to: '?@gmail.com',
-        subject: 'meh - ' + utils.trim(subject),
-        html: body
-    };    
-
-    // create transporter with SMTP credentials
-    var transporter = nodemailer.createTransport({
-        service: 'Gmail',
-        auth: {
-            user: '?',
-            pass: '?'
-        }
-    });
-
-    // send mail
-    transporter.sendMail(mailOptions, function(error, info){
-        if(error){
-            console.log(error);
-        }else{
-            console.log('Message sent: ' + info.response);
-        }
-    });
-	} else {
-		// error logging
-		console.error('status code' + response.statusCode);
-		console.error(error);
-	}
+// create transporter with SMTP credentials
+var transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+        user: process.env.smtpUser,
+        pass: process.env.smtpPassword
+    }
 });
+
+// create e-mail message
+var mailOptions = {
+    from: process.env.mehFrom,
+    to: process.env.mehTo
+};
+
+// send mail
+var sendMail = function() {
+  transporter.sendMail(mailOptions, function(error, info) {
+      if (error) {
+          console.log(error);
+      } else {
+          console.log('Message sent: ' + info.response);
+      }
+  });
+};
+
+var currentMeh = {};
+var fetchAndNotify = function() {
+  request('http://meh.com', function (error, response, html) {
+    if (!error && response.statusCode === 200) {
+      // meh parsing
+      var $ = cheerio.load(html);
+      var buyButton = $('div#hero-buttons button.buy-button');
+      var subject = $('section.features h2').text(),
+          body  = '<div><a href="' + buyButton.attr('href') + '">' + buyButton.text() + '</a></div>';
+          body += $('div#price-check').html();
+          body += $('section.features').html();
+          body += $('section.story').html();
+
+      // populate mail with meh deal
+      mailOptions.subject = 'meh - ' + utils.trim(subject);
+      mailOptions.html = utils.trim(body);
+    } else {
+      // error logging
+      console.error('status code' + response.statusCode);
+      console.error(error);
+
+      // populate mail with error
+      mailOptions.subject = 'meh - error';
+      mailOptions.text = 'Failed to parse meh for notification: HTTP ' + response.statusCode;
+    }
+
+    // send the mail
+    sendMail();
+
+    // capture the current meh in an object
+    currentMeh = mailOptions;
+    currentMeh.to = '[redacted]';
+  });
+}();
+
+// setup the schedule
+later.date.localTime();
+var schedule = later.parse.text('at 12:15am'),
+    timer = later.setInterval(fetchAndNotify, schedule),
+    occurrences = later.schedule(schedule);
+
+// handles incoming requests to the server
+var _requestHandler = function(request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.write('waiting...\n');
+  response.write('next notification at: ' + occurrences.next(1, new Date()));
+  response.write('\n\nmost recent meh notification:\n');
+  response.write(JSON.stringify(currentMeh, null, 4));
+  response.end();
+};
+
+// fire up the status server
+var server = http.createServer().addListener('request', _requestHandler).listen(process.env.PORT);
